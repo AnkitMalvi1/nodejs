f7c72b1cbb0f6f172a8119f233b3fc122142b859
AnkitMalvi1 <ankitmalviya06219@gmail.com>
2025-01-24 00:49:16 +0530

    add some files

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("starting the nodejs");
+const fs = require('fs');
+const http = require('http');
+
+// fs.readFile('example.txt', 'utf8', (err, data) => {
+//     if(err){
+//         console.error(err);
+//         return;
+//     }
+//     console.log(data);
+// })
+
+// const content = 'Hello, NodeJs';
+
+// fs.writeFile('output.txt', content, (err) => {
+//     if(err){
+//         console.error(err);
+//         return;
+//     }
+//     console.log('File written successfully');
+// })
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    
+})
