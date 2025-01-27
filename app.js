215e445715c01b39a3e8b182d76df49bc3066be3
AnkitMalvi1 <ankitmalviya06219@gmail.com>
2025-01-27 16:00:24 +0530

    add some files

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
 const http = require('http');
+const path = require('path');
+const os = require('os');
+const crypto = require('crypto');
+const sayHello = require('./greetings');
+const math = require('./math');
+const lodash = require('lodash');
 
 // fs.readFile('example.txt', 'utf8', (err, data) => {
 //     if(err){
@@ -20,7 +26,50 @@ const http = require('http');
 // })
 
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    
-})
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello, World');
+// })
+
+// server.listen(3000, () => {
+//     console.log('Server listening at http://localhost:3000');
+// })
+
+
+// const directory = '/user/local';
+// const fileName = 'example.txt';
+
+// const fullPath = path.join(directory, fileName);
+
+// console.log(fullPath);
+
+
+// console.log('Platform: ', os.platform());
+// console.log('CPU Arch: ', os.arch());
+// console.log('Total Memory: ', os.totalmem());
+// console.log('Free Memory: ', os.freemem());
+
+
+// const myUrl = new URL('https://example.com:8080/path/name?query=hello#hash');
+// console.log('Host', myUrl.host);
+// console.log('Pathname', myUrl.pathname);
+// console.log('Search Params', myUrl.searchParams.get('query'));
+
+
+// const hash = crypto.createHash('sha256');
+// hash.update('Hello, World');
+
+// console.log(hash.digest('hex'));
+
+
+// const message = sayHello('Developers');
+// console.log(message);
+// console.log(math.add(5,3));
+// console.log(math.subtract(5,3));
+
+
+// const numbers = [1, 2, 3, 4, 5];
+// const reversed = lodash.reverse(numbers);
+
+// console.log(reversed);
